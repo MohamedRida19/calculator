aba997c2303d18cc97498d8e40ed4b697fd67fba
mohamed rida <rayanereda90@gmail.com>
2023-08-14 14:45:06 +0000

    creating calculator function

---
+++ calc.py
@@ -0,0 +1,14 @@
+def calc(a,b):
+    choice = input("chose the operation: +|-|*|/ ")
+    if choice == "+":
+        x= a+b
+        print("the result is:",x)
+    if choice == "-":
+        x= a-b
+        print ("the result is:",x)
+    if choice == "*":
+        x= a*b
+        print("the result is:",x)
+    if choice == "/":
+        x= a/b
+        print ("the result is:",x)
